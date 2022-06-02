ed9780554cf40c7f24f043d49294c10ba64987ac
William Lee <wkl@explainedbycats.com>
2022-06-02 16:54:23 -0400

    Support requested blocks format

--- mentos/py/util/block.py
+++ mentos/py/util/block.py
@@ -1,7 +1,8 @@
 import asyncio
-import json
+import re
 
 from datetime import datetime
+from operator import attrgetter
 from typing import Any, Dict
 
 from mentos.py.freshdesk.client import (
@@ -10,14 +11,20 @@ from mentos.py.freshdesk.client import (
 
 import mentos.py.freshdesk.models as fdmodels
 
-import logging
-logger = logging.getLogger("uvicorn")
-
 
 class FullBlockCreator:
     def __init__(self, freshdesk: FreshDeskClient, access_url: str):
         self.client = freshdesk
         self.access_url = access_url
+
+    def format_body(self, text: str) -> str:
+        """Reply bodies get some rather ugly formatting so this tries to do some
+        pretty formatting without trying to touch the html-ified "body" that is
+        also sent via API calls like conversations or tickets."""
+        # basic rules - if something has more than 3 spaces in a row, assume
+        # it's just a linebreak.
+        lines = re.split(r"\s{2,}", text)
+        return "\n\n".join(lines)
 
     def create_date(self, date: datetime, title: str) -> str:
         ts = int(date.timestamp())
@@ -39,13 +46,13 @@ class FullBlockCreator:
 
         # tickets provide a bunch of identifiers that need to be reified
         # into additional objects
-        agent, req, group, dept = await asyncio.gather(
+        agent, req, group, dept, convos = await asyncio.gather(
             self.client.get_agent(ticket.responder_id),
             self.client.get_agent(ticket.requester_id),
             self.client.get_agent_group(ticket.group_id),
-            self.client.get_department(ticket.department_id)
+            self.client.get_department(ticket.department_id),
+            self.client.get_conversations(ticket_id)
         )
-        logger.debug(agent)
 
         header = {
             "type": "header",
@@ -71,8 +78,12 @@ class FullBlockCreator:
                 {
                     "type": "mrkdwn",
                     "text": f"*Ticket:*\n<{ticket_url}|{ident}>"
-                },
-                # ticket URL goes here...
+                }
+            ]
+        }
+
+        if verbose:
+            info_sections["fields"].extend([
                 {
                     "type": "mrkdwn",
                     "text": submitted
@@ -97,20 +108,52 @@ class FullBlockCreator:
                     "type": "mrkdwn",
                     "text": f"*Assigned Tech:*\n{agent.first_name} {agent.last_name}"
                 }
-            ]
-        }
+            ])
 
         description = {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": f"*Description*:\n{ticket.description_text.strip()}"
-            }
+            "mrkdwn_in": ["pretext", "text"],
+            "pretext": "*Description*",
+            "color": "#ff9933",
+            "text": self.format_body(ticket.description_text)
         }
-        blocks = [header, divider, info_sections, divider, description]
 
-        final = {
+        # for last update, sort the conversations by created (probably?)
+        replies = sorted(convos, key=attrgetter("created_at"), reverse=True)
+        last_public = next((r for r in replies if not r.private), None)
+
+        if last_public:
+            if last_public.user_id == ticket.requester_id:
+                reply_from = f"Client: {req.first_name} {req.last_name}"
+                color = "#439fe0"
+            elif last_public.user_id == agent.id:
+                reply_from = f"Tech: {agent.first_name} {agent.last_name}"
+                color = "#3cba54"
+            else:
+                reply_from = "CC"
+                color = "#f4c20d"
+
+            reply_attachment = {
+                "mrkdwn_in": ["pretext", "text"],
+                "pretext": "*Latest Reply*",
+                "color": color,
+                "author_name": reply_from,
+                "text": self.format_body(last_public.body_text)
+            }
+        else:
+            reply_attachment = {
+                "mrkdwn_in": ["text"],
+                "color": "#db3236",
+                "text": "**No replies to show**"
+            }
+
+        blocks = [header, divider, info_sections, divider]
+        if verbose:
+            attachments = [description, reply_attachment]
+        else:
+            attachments = [reply_attachment]
+
+        return {
             "response_type": "in_channel" if not ephemeral else "ephemeral",
-            "blocks": blocks
+            "blocks": blocks,
+            "attachments": attachments
         }
-        return json.dumps(final)
